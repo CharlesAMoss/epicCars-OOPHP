820ae7c86ed0b2df5e2bdfdd2217dc772e4816ac
Alex Costa & charlesamoss <student@epicodus.com>
2015-08-03 12:08:13 -0700

    Made form

--- car.php
+++ car.php
@@ -1,4 +1,7 @@
 <?php
+$user_price = $_GET["price"];
+
+
 class Car
 {
     public $make_model;
@@ -25,3 +28,41 @@ $ford = new Car("Truck", 3000.00, 30, "image/ford.jpg");
 $toyoda = new Car("4 door", 14000.00, 60000, "image/yoda.jpg");
 
 $cars = array($honda, $ford, $toyoda);
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Cars Drive Fast</title>
+
+  <style>
+    img{
+      max-width: 300px;
+    }
+  </style>
+  </head>
+  <body>
+
+    <h1> I'm the h1 </h1>
+
+
+    <?php
+
+
+        foreach ($cars as $car) {
+            $miles = $car->getMiles();
+            if ($car->price <= $user_price) {
+                echo "<img src='$car->image'> $car->make_model " . "$" . "$car->price" . " " . "$miles ";
+            }else{
+                continue;
+            }
+
+
+      }
+
+      ?>
+
+
+
+  </body>
+</html>
